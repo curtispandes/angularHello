4e4bb164ce9d7583f33a5bb01e57c5054d27d4fe
curtispandes <curtispandes@gmail.com>
2023-09-06 15:04:04 -0700

    Step 4 Add Services check, unfinished

--- angular-tour-of-heroes/src/app/message.service.spec.ts
+++ angular-tour-of-heroes/src/app/message.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
 
-import { MessageService } from './message.service';
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  messages: string[] = [];
 
-describe('MessageService', () => {
-  let service: MessageService;
+  add(message: string) {
+    this.messages.push(message);
+  }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MessageService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  clear() {
+    this.messages = [];
+  }
+}
