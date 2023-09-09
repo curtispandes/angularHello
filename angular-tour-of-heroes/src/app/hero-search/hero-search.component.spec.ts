d4561321eb4a20a3ba4b1417b0dded76a9399875
curtispandes <curtispandes@gmail.com>
2023-09-08 18:31:32 -0700

    dashboard somewhat fixed, still missing top heroes buttons

--- angular-tour-of-heroes/src/app/hero-search/hero-search.component.spec.ts
+++ angular-tour-of-heroes/src/app/hero-search/hero-search.component.spec.ts
@@ -1,21 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit } from '@angular/core';
 
-import { HeroSearchComponent } from './hero-search.component';
+import { Observable, Subject } from 'rxjs';
 
-describe('HeroSearchComponent', () => {
-  let component: HeroSearchComponent;
-  let fixture: ComponentFixture<HeroSearchComponent>;
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HeroSearchComponent]
-    });
-    fixture = TestBed.createComponent(HeroSearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: [ './hero-search.component.css' ]
+})
+export class HeroSearchComponent implements OnInit {
+  heroes$!: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) {}
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
+}
